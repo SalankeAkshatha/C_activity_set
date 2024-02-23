e94c9c15512d67fa00e2d9cbdb1136dbd47a794b
SalankeAkshatha <sakshatha.bsc23@rvu.edu.in>
2024-02-23 14:03:21 +0530

    test

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b,c;
+printf("Enter one number:");
+scanf("%d",&a);
+printf("Enter one number:");
+scanf("%d",&b);
+c = a+b;
+printf("The sum of %d %d is %d \n",a,b,c);
+return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int input();
+int add(int a,int b);
+void output(int a,int b ,int sum);
+
+int main()
+{
+    int a,b,sum;
+    a=input();
+    b=input();
+    sum=add(a,b);
+    output(a,b,sum);
+    return 0;
+}
+int input()
+{
+    int x;
+    printf("%d",&x);
+    return x;
+}
+int add(int a,int b)
+{
+    return a+b;
+}
+void output(int a,int b,int sum)
+{
+    printf("The sum of %d + %d is %d \n",a,b,sum);
+}
